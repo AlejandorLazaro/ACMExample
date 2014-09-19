8be2cbfbf732ffab1647aad5e72d3b783322f8d0
xX.SUPREME.HACK0R.Xx <dashie@bestpone.org>
2014-09-19 17:00:59 -0500

    Use C++ exception handling, not Java.

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <exception>
 
 int main()
 {
   std::cout << "Hello ACM!";
 
-  // If this was java, I could've done this:
-  //    Exception up = new Exception(“Something is really wrong.”);
-  //    throw up; //ha ha
+  int up = 4;
+
+  throw up;
 }
